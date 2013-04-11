83678169784b2a36a7bd7aa136ca5efee1cfb31a
unknown <lorne_G@TRU-D-2009-0909.mcw.com>
2013-04-11 10:23:28 -0300

    Fix for issue #5

--- page.dialplaninjection.php
+++ page.dialplaninjection.php
@@ -14,13 +14,26 @@ $dispnum = 'dialplaninjection'; //used for switch on config.php
 
 ?>
 
-
 <div class="rnav">
+
 <?php 
+
 $contexts = dialplaninjection_getinjections();
 drawListMenu($contexts, $skip, $type, $display, $extdisplay, _("Injection"));
+
 ?>
 </div>
+
+<?php
+// check to see if user has automatic updates enabled in FreePBX settings
+$cm =& cronmanager::create($db);
+$online_updates = $cm->updates_enabled() ? true : false;
+
+// check dev site to see if new version of module is available
+if ($online_updates && $foo = dialplaninjection_vercheck()) {
+	echo "<br>A <b>new version of this module is available</b> from the <a target='_blank' href='http://pbxossa.org'>PBX Open Source Software Alliance</a><br>";
+	}
+?>
 
 <!--
 			<tr><td colspan="2"><br><h5><?php echo _("Destination if no answer")?>:<hr></h5></td></tr>
